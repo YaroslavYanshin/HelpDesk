6e541d878acd86541209dc18af78c04556caabfe
YaroslavYanshyn <38620931+YaroslavYanshyn@users.noreply.github.com>
2019-01-28 18:14:17 +0300

    Created models: User, Role,Request

---
+++ HelpDesk/Enums/RequestPriority.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelpDesk.Enums
+{
+    public enum RequestPriority
+    {
+        Low = 1,
+        Medium = 2,
+        High = 3,
+        Critical = 4
+    }
+}
---
+++ HelpDesk/Enums/RequestStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelpDesk.Enums
+{
+    public enum RequestStatus
+    {
+        Open = 1,
+        Distributed = 2,
+        Proccesing = 3,
+        Checking = 4,
+        Closed = 5
+    }
+}
