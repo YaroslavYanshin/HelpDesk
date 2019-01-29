232bdfaf68978a6c2e3104fc3c46c95343ddb945
YaroslavYanshyn <38620931+YaroslavYanshyn@users.noreply.github.com>
2019-01-29 12:46:13 +0300

    classes added to the model: Category, Department,Activ

---
+++ HelpDesk/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelpDesk.Models
+{
+    // Категория проблемы(неисправности)
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Название категории")]
+        [MaxLength(50, ErrorMessage = "Превышена максимальная длина записи")]
+        public string Name { get; set; }
+    }
+}
---
+++ HelpDesk/Models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelpDesk.Models
+{
+    //модель отдела(цеха)
+    public class Department
+    {
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Название отдела(цеха)")]
+        [MaxLength(50,ErrorMessage = "Привышена максимальная длина записи")]
+        public string Name { get; set; }
+
+    }
+}
